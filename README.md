425479e109f1b4e1286b77a29c81b6a5cecaa8c5
shinework <adrien.baptiste@gmail.com>
2016-06-24 15:01:42 +0200

    Added Doc

--- README.md
+++ README.md
@@ -1,4 +1,41 @@
-sf-api-project
+API
 ==============
 
-A Symfony project created on June 24, 2016, 9:17 am.
+### `GET` /users/{userId}/places.{_format} ###
+
+_Retrieves a user places_
+
+#### Requirements ####
+
+**_format**
+
+  - Requirement: json|xml|html
+**userId**
+
+
+
+### `POST` /users/{userId}/places.{_format} ###
+
+_Create a user place_
+
+#### Requirements ####
+
+**_format**
+
+  - Requirement: json|xml|html
+**userId**
+
+
+
+### `DELETE` /users/{userId}/places/{placeId}.{_format} ###
+
+_Delete a user place_
+
+#### Requirements ####
+
+**_format**
+
+  - Requirement: json|xml|html
+**userId**
+
+**placeId**
--- src/AppBundle/Controller/PlacesController.php
+++ src/AppBundle/Controller/PlacesController.php
@@ -6,12 +6,21 @@ use AppBundle\Entity\Place;
 use AppBundle\Entity\User;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use FOS\RestBundle\View\View;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 
 class PlacesController extends FOSRestController
 {
     /**
+     * Get user places
+     *
+     * @ApiDoc(
+     *  resource=true,
+     *  description="Retrieves a user places"
+     * )
+     *
      * @Rest\View()
      */
     public function getUsersPlacesAction($userId)
@@ -20,6 +29,26 @@ class PlacesController extends FOSRestController
         return ['places' => $user->getPlaces()];
     }
 
+    /**
+     * @ApiDoc(
+     *  resource=true,
+     *  description="Create a user place"
+     * )
+     *
+     * @Rest\View()
+     */
+    public function postUsersPlacesAction(Request $request, $userId)
+    {
+
+    }
+
+    /**
+     * @ApiDoc(
+     *  description="Delete a user place"
+     * )
+     *
+     * @Rest\View()
+     */
     public function deleteUsersPlacesAction($userId, $placeId)
     {
         $user = $this->getDoctrine()->getRepository(User::class)->findOneById($userId);
